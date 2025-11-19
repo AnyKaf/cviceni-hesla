8db6d8c78bafed4e63d0768e11610973a00de40e
Anežka Kafková <anezka@zzaba.cz>
2025-11-19 10:30:05 +0100

    hotovo

--- index.js
+++ index.js
@@ -27,3 +27,15 @@ const strongPassword = (len) => {
 
   return result;
 };
+
+const createAccount = (user, generatePassword) => {
+    // const password = generatePassword(9)
+    return `<p>Uživatel ${user} s heslem ${generatePassword(9)}</p>`
+}
+
+document.body.innerHTML += `
+	<p>${createAccount('Míša', weakPassword)}</p>
+	<p>${createAccount('Řízek', mediumPassword)}</p>
+	<p>${createAccount('Mápodčepicí', strongPassword)}</p>
+`;
+
